7a3a2d614384301da1b210da5ff1ca4f4cfd7fc1
Alex Yu <alex.yu.dev@gmail.com>
2025-05-16 19:45:25 +0300

    feat(api): instantiation

---
+++ api.go
@@ -0,0 +1,60 @@
+package privatbank
+
+import (
+	"io"
+	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+const _API = "https://acp.privatbank.ua/api"
+
+type API struct {
+	Logger io.Writer
+	agent  *apiHttpAgent
+	_EOL   string
+}
+
+type APIOptions struct {
+	Token    string
+	Encoding string
+	Logger   io.Writer
+}
+
+func NewAPI(options APIOptions) *API {
+	eol := "\n"
+
+	if runtime.GOOS == "windows" {
+		eol = "\r\n"
+	}
+
+	return &API{
+		agent:  newAPIHttpAgent(options.Token, options.Encoding),
+		Logger: options.Logger,
+		_EOL:   eol,
+	}
+}
+
+func (a *API) logResponse(resp *http.Response) {
+	text := resp.Status + " " +
+		resp.Proto + " " +
+		resp.Request.Method + " " +
+		resp.Request.URL.String() + " [" +
+		strconv.FormatInt(resp.ContentLength, 10) + "b]" + a._EOL +
+		"Header" + a._EOL
+
+	for key, item := range resp.Header {
+		text += "  " + key + ": " + strings.Join(item, "; ") + a._EOL
+	}
+
+	if cookies := resp.Cookies(); len(cookies) > 0 {
+		text += a._EOL + "Cookies"
+
+		for _, item := range resp.Cookies() {
+			text += "  " + item.String() + a._EOL
+		}
+	}
+
+	a.Logger.Write([]byte(text + a._EOL))
+}
