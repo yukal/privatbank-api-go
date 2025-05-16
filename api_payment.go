942528019f22f608d536bdf630b1a1774ef4b616
Alex Yu <alex.yu.dev@gmail.com>
2025-05-16 20:02:33 +0300

    feat(api): get receipt

--- api_payment.go
+++ api_payment.go
@@ -1,6 +1,8 @@
 package privatbank
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 )
 
@@ -9,6 +11,26 @@ import (
 // Отримання інформації по платежу
 func (a *API) GetPaymentInfo(paymentRef string) (resp *http.Response, err error) {
 	if resp, err = a.agent.requestGet("/proxy/payment/get?ref=" + paymentRef); err != nil {
+		return
+	}
+
+	a.logResponse(resp)
+
+	return
+}
+
+func (a *API) GetReceipt(account, reference, refn string) (resp *http.Response, err error) {
+	body := bytes.NewBuffer(
+		[]byte(
+			fmt.Sprintf(`{"transactions":[{"account": "%s", "reference": "%s", "refn": "%s"}],"perPage": 4}`,
+				account,
+				reference,
+				refn,
+			),
+		),
+	)
+
+	if resp, err = a.agent.requestPostOctet("/paysheets/print_receipt", body); err != nil {
 		return
 	}
 
